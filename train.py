2a9ecd1a3830183c673f59f47b656abf95c2d346
Jean-KOUAGOU <jeank@aims.ac.za>
2022-09-05 17:29:58 +0200

    train script to reproduce training of nces

---
+++ train.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import torch, pandas as pd, numpy as np
+import os, json
+import argparse
+import random
+
+seed = 42
+random.seed(seed)
+os.environ['PYTHONHASHSEED'] = str(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+from utils.experiment import Experiment
+from utils.data import Data
+import json
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    elif v.lower() in ['t', 'true', 'y', 'yes', '1']:
+        return True
+    elif v.lower() in ['f', 'false', 'n', 'no', '0']:
+        return False
+    else:
+        raise ValueError('Invalid boolean value.')
+        
+parser = argparse.ArgumentParser()
+parser.add_argument('--kbs', type=str, nargs='+', default=['carcinogenesis'], choices=['carcinogenesis', 'mutagenesis', 'semantic_bible', 'vicodi'], help='Knowledge base name')
+parser.add_argument('--models', type=str, nargs='+', default=['LSTM', 'GRU', 'SetTransformer'], help='Neural models')
+parser.add_argument('--learner_name', type=str, default="SetTransformer", choices=['LSTM', 'GRU', 'SetTransformer'], help='Neural model')
+parser.add_argument('--knowledge_base_path', type=str, default="", help='Path to KB owl file')
+parser.add_argument('--path_to_csv_embeddings', type=str, default="", help='KB embedding path')
+parser.add_argument('--learning_rate', type=float, default=1e-3, help='Learning rate')
+parser.add_argument('--embedding_dim', type=int, default=40, help='Number of embedding dimensions')
+parser.add_argument('--input_size', type=int, default=40, help='Number of embedding dimensions in the input')
+parser.add_argument('--num_workers', type=int, default=8, help='Number of workers to use to load training data')
+parser.add_argument('--proj_dim', type=int, default=128, help='The projection dimension for examples')
+parser.add_argument('--num_inds', type=int, default=32, help='Number of induced instances')
+parser.add_argument('--num_heads', type=int, default=4, help='Number of attention heads')
+parser.add_argument('--num_seeds', type=int, default=1, help='Number of seed components in the output')
+parser.add_argument('--num_examples', type=int, default=1000, help='Total number of examples for concept learning')
+parser.add_argument('--ln', type=str2bool, default=False, help='Whether to use layer normalization')
+parser.add_argument('--decay_rate', type=float, default=0.0, help='Decay rate for the optimizer')
+parser.add_argument('--grad_clip_value', type=float, default=5.0, help='Gradient clip value')
+parser.add_argument('--opt', type=str, default='Adam', help='Name of the optimizer to use')
+parser.add_argument('--max_num_atom_repeat', type=int, default=8, help='Maximum number of an atom repetition in a given class expression')
+parser.add_argument('--rnn_n_layers', type=int, default=2, help='Number of recurrent network layers')
+parser.add_argument('--alpha', type=float, default=10.0, help='Upper bound for scoring atoms/tokens')
+parser.add_argument('--lbr', type=float, default=0.8, help='Lower bound rate')
+parser.add_argument('--use_adaptive_bounds', type=str2bool, default=False, help='Whether to rescale the lower and upper bounds based on class expression lengths')
+parser.add_argument('--max_length', type=int, default=32, help='Maximum length of class expressions')
+parser.add_argument('--drop_prob', type=float, default=0.1, help='Dropout rate in neural networks')
+parser.add_argument('--epochs', type=int, default=1000, help='Number of training epochs')
+parser.add_argument('--batch_size', type=int, default=256, help='Training batch size')
+parser.add_argument('--cross_validate', type=str2bool, default=False, help='Whether to use a 10-fold cross-validation setting')
+parser.add_argument('--test', type=str2bool, default=True, help='Whether to evaluate the concept synthesizer on the test data during training')
+parser.add_argument('--final', type=str2bool, default=False, help='Whether to train the concept synthesizer on test+train data')
+
+args = parser.parse_args()
+
+for kb in args.kbs:
+    data_train_path = f"datasets/{kb}/Train_data/Data.json"
+    with open(data_train_path, "r") as file:
+        data_train = json.load(file)
+
+    data_test_path = f"datasets/{kb}/Test_data/Data.json"
+    with open(data_test_path, "r") as file:
+        data_test = json.load(file)
+        
+    args.knowledge_base_path = f"datasets/{kb}/{kb}.owl"
+    args.path_to_csv_embeddings = f"embeddings/{kb}/ConEx_entity_embeddings.csv"
+    #kwargs = {"learner_name": "SetTransformer", "emb_model_name": "",
+    #          "pretrained_concept_synthesizer": base_path+f"Datasets/{kb}/Model_weights/GRU.pt", 
+    #          "path_to_csv_embeddings": base_path+f"Embeddings/{kb}/ConEx_entity_embeddings.csv",
+    #          "learning_rate": args.lr, "decay_rate": args.decay_rate, 'grad_clip_value': args.grad_clip_value, 
+    #          "path_to_triples": path_to_triples, 'max_num_atom_repeat': args.max_num_atom_repeat,
+    #          'alpha': args.alpha, 'lbr': args.lbr,
+    #          'max_length': args.max_length, 'num_workers': args.num_workers, 'use_adaptive_bounds': args.use_adaptive_bounds,
+    #          "embedding_dim": args.embedding_dim, "num_entities": len(triples.entities),
+    #          "num_relations": len(triples.relations), "num_examples": args.num_examples, 'drop_prob': args.drop_prob,
+    #          "rnn_n_layers": args.rnn_n_layers, 'input_size': 40, 'rnn_n_hidden': args.rnn_n_hidden,
+    #          "proj_dim": args.proj_dim, 'num_inds': args.num_inds, 'num_heads': args.num_heads, 'ln': args.ln, 'num_seeds': args.num_seeds}
+    
+    experiment = Experiment(args)
+    setattr(experiment, 'kb', kb)
+    data_train, data_test = list(data_train.items()), list(data_test.items())
+
+    final = args.final
+    test = args.test
+    cross_validate = args.cross_validate
+    record_runtime = True
+    save_model = True
+    if args.final:
+        data_train = data_train + data_test
+        test = False
+        cross_validate = False
+    experiment.train_all_nets(args.models, data_train, data_test, epochs=args.epochs, batch_size=args.batch_size, kf_n_splits=10, 
+                              cross_validate=cross_validate, test=test, save_model = save_model,
+                              optimizer = args.opt, record_runtime=record_runtime, final=final)
